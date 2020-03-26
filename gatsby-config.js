3566e8b1b457525cfbe36f9b07c5e336b91650d7
Nicholas Duffy <duffn@outlook.com>
2020-03-25 19:52:03 -0600

    weshipit.xyz

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: `Hey I ship that Me too`,
     description: `Hey I ship that Me too`,
     author: `@duffn`,
-    gifUrl: `https://weshipit.netlify.com/weshipit.gif`,
+    gifUrl: `https://weshipit.xyz/weshipit.gif`,
   },
   plugins: [`gatsby-plugin-react-helmet`],
 }
